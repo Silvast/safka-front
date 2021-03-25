020c1cd3c5fe4f478229f5df3d8c0a4fdcf52aa1
Anne-Mari Silvast <anne-mari.silvast@yle.fi>
2021-03-25 20:02:07 +0200

    fixed receipt-url

--- src/cljs/safka_front/config.cljs
+++ src/cljs/safka_front/config.cljs
@@ -1,9 +1,8 @@
 (ns safka-front.config
-(:require [safka-front.coremacros :as coremacros])
   )
 
 (def debug?
   ^boolean goog.DEBUG)
 
 
-(def conf {:url "localhost"})
+(def conf {:url (.. js/window -location -host)})
